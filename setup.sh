ec76281f8c2f954a715dd7f23ce122b57047122b
ktlwap <149824361+ktlwap@users.noreply.github.com>
2024-03-20 21:35:00 +0100

    Add setup dependencies

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 sudo apt-get update
 
-echo "Install .NET SDK"
+echo "Install .NET SDK and Tools"
 sudo apt-get install dotnet-sdk-8.0
+dotnet tool install --global ClangSharpPInvokeGenerator
 
 echo "Install build dependencies"
 sudo apt-get install python3 meson libwayland-dev wayland-protocols libegl-dev \
                      libgles-dev libvulkan-dev libdrm-dev libgbm-dev \
                      libxkbcommon-dev libudev-dev libpixman-1-dev libseat-dev \
-                     hwdata
+                     hwdata clang llvm
