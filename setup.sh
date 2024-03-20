5e6cb99352f3859be98644291e0ab267c0ac8a05
ktlwap <149824361+ktlwap@users.noreply.github.com>
2024-03-20 22:04:19 +0100

    Add libseat dependency

--- setup.sh
+++ setup.sh
@@ -9,4 +9,4 @@ sudo apt-get install python3 meson libwayland-dev wayland-protocols libegl-dev \
                      libgles-dev libvulkan-dev libdrm-dev libgbm-dev \
                      libxkbcommon-dev libudev-dev libpixman-1-dev libseat-dev \
                      hwdata clang llvm libwayland-server0 libwayland-cursor0 \
-                     libwayland-client0
+                     libwayland-client0 libseat-dev
