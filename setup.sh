3f18e1d2c06cbb287d6a130d65439b3c537b8051
ktlwap <149824361+ktlwap@users.noreply.github.com>
2024-03-20 21:30:05 +0100

    Fix broken dependencies

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ echo "Install .NET SDK"
 sudo apt-get install dotnet-sdk-8.0
 
 echo "Install build dependencies"
-sudo apt-get install python3 python3-mesonpy libwayland-dev wayland-protocols \
-                     libegl-dev libgles-dev libvulkan-dev libdrm-dev libgbm-dev \
+sudo apt-get install python3 meson libwayland-dev wayland-protocols libegl-dev \
+                     libgles-dev libvulkan-dev libdrm-dev libgbm-dev \
                      libxkbcommon-dev libudev-dev libpixman-1-dev libseat-dev \
-                     hwdata libdisplay-info-dev libliftoff-dev
+                     hwdata
